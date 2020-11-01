28c7cd2e0197797dfbc950456f7e5516045de982
NevilleAdrian <neville.chukumah@gmail.com>
2020-11-01 11:40:13 +0100

    Admin dashboard table && sidebar

--- transmonitor/src/component/body/index.js
+++ transmonitor/src/component/body/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import MainBody from '../mainbody'
+import SideBar from '../sidebar'
+import "./index.css"
 
 export default function Body() {
     return (
-        <div>
-            <p>Body</p>
+        <div className="row top-row" >
+           <SideBar/>
+           <MainBody/>
         </div>
     )
 }
---
+++ transmonitor/src/data/data.js
@@ -0,0 +1,171 @@
+import Ojo from "../assets/Ojo.svg"
+import Overview from "../assets/Overview.svg"
+import Wallet from "../assets/Wallet.svg"
+import Wallet2 from "../assets/Wallet2.svg"
+import Wallet3 from "../assets/Wallet3.svg"
+import Radio from "../assets/Radio.svg"
+import Order from "../assets/Order.svg"
+import Order2 from "../assets/Order2.svg"
+import Order3 from "../assets/Order3.svg"
+import Profile from "../assets/Profile.svg"
+import Vw from "../assets/Vw.svg"
+
+
+
+
+export const navData = [
+    {
+        image: Ojo,
+        name: 'Olawaleke Ojo'
+    }
+]
+
+export const Main = [
+    {
+        name: 'Overview',
+        image: Overview
+    }
+]
+
+export const Payment = [
+    {
+        name: 'All Payments',
+        image: Wallet
+    },
+    {
+        name: 'Reconciled Payments',
+        image: Wallet2
+    },
+    {
+        name: 'Un - Reconciled Payments',
+        image: Wallet3
+    },
+     {
+        name: 'Manual Settlement',
+        image: Radio
+    },
+   
+   
+]
+
+export const Orders = [
+    {
+        name: 'All Orders',
+        image: Order
+    },
+    {
+        name: 'Pending Orders',
+        image: Order3
+    },
+    {
+        name: 'Reconciled Payments',
+        image: Order2
+    },
+ 
+   
+]
+
+
+
+export const Profiles = [
+    {
+        name: 'Merchant Profile',
+        image: Profile
+    },
+  
+]
+
+export const Transactions = [
+    {
+        name: 'Daily Transaction Volume',
+        number: '2,342'
+    },
+    {
+        name: 'Daily Transaction Value',
+        number: '₦4,000,000'
+    },
+    {
+        name: 'Total Transaction Volume',
+        number: '452,000'
+    },
+    {
+        name: 'Total Transaction Value',
+        number: '₦4,000,000'
+    },
+   
+]
+
+export const Table = [
+    {
+        image: Vw,
+        name: "Apple Mac Book 15” 250 SSD 12GB",
+        price: "$73430",
+        no: "$73430",
+        time: "12:30",
+        status: "Pending",
+        color: "#EBC315"
+    },
+    {
+        image: Vw,
+        name: "Apple Mac Book 15” 250 SSD 12GB",
+        price: "$73430",
+        no: "$73430",
+        time: "12:30",
+        status: "Reconciled",
+        color: "#27AE60"
+    },
+    {
+        image: Vw,
+        name: "Apple Mac Book 15” 250 SSD 12GB",
+        price: "$73430",
+        no: "$73430",
+        time: "12:30",
+        status: "Un-Reconciled",
+        color: "#C4C4C4"
+    },
+    {
+        image: Vw,
+        name: "Apple Mac Book 15” 250 SSD 12GB",
+        price: "$73430",
+        no: "$73430",
+        time: "12:30",
+        status: "Reconciled",
+        color: "#27AE60"
+    },
+    {
+        image: Vw,
+        name: "Apple Mac Book 15” 250 SSD 12GB",
+        price: "$73430",
+        no: "$73430",
+        time: "12:30",
+        status: "Pending",
+        color: "#EBC315"
+    },
+    {
+        image: Vw,
+        name: "Apple Mac Book 15” 250 SSD 12GB",
+        price: "$73430",
+        no: "$73430",
+        time: "12:30",
+        status: "Un-Reconciled",
+        color: "#C4C4C4"
+    },
+    {
+        image: Vw,
+        name: "Apple Mac Book 15” 250 SSD 12GB",
+        price: "$73430",
+        no: "$73430",
+        time: "12:30",
+        status: "Pending",
+        color: "#EBC315"
+    },
+    {
+        image: Vw,
+        name: "Apple Mac Book 15” 250 SSD 12GB",
+        price: "$73430",
+        no: "$73430",
+        time: "12:30",
+        status: "Un-Reconciled",
+        color: "#C4C4C4"
+    },
+]
